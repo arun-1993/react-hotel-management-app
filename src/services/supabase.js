377946409234e0915382b06617072a896050be3c
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-12-04 01:34:13 +0530

    feat: connected the app to the supabase backend with read-only public access for now

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ipiffschofrbwziydegz.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwaWZmc2Nob2ZyYnd6aXlkZWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3NzMyMDIsImV4cCI6MjAxMzM0OTIwMn0.u93AlmiqUsJJjIaXWwuMIvwOVGduYSxe_2sfW7RZlOw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
