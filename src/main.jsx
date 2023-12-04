c59a8f47fcddd2bd7917b1c85826f72cf0b2c5bd
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-12-04 18:37:17 +0530

    feat: set up react query and react query devtools for remote state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import GlobalStyle from "./styles/GlobalStyles.js";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 100,
+        },
+    },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <GlobalStyle />
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
+            <GlobalStyle />
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 );
