973b73edc114bba7f937bf04340c42caffde628b
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-12-18 16:31:07 +0530

    feat: added a functionality to upload test data in the database
    - It is placed in the Sidebar
    - It will delete all pre-existing data and uploads predefined test data to the database

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
-import { formatDistance, parseISO } from "date-fns";
+import { differenceInDays, formatDistance, parseISO } from "date-fns";
 
 export const formatCurrency = (value) =>
     new Intl.NumberFormat("en", { style: "currency", currency: "USD" }).format(
@@ -9,3 +9,6 @@ export const formatDistanceFromNow = (dateStr) =>
     formatDistance(parseISO(dateStr), new Date(), { addSuffix: true })
         .replace("about ", "")
         .replace("in", "In");
+
+export const subtractDates = (dateStr1, dateStr2) =>
+    differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
