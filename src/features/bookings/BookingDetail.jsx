35c235f4d8f42444f8149e19216f12db50eb247a
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-12-23 06:07:50 +0530

    feat: added error boundary, defaulting dark mode to system default and handling invalid booking id

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -10,6 +10,7 @@ import Button from "../../ui/Button";
 import ButtonGroup from "../../ui/ButtonGroup";
 import ButtonText from "../../ui/ButtonText";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Empty from "../../ui/Empty";
 import Heading from "../../ui/Heading";
 import Modal from "../../ui/Modal";
 import Row from "../../ui/Row";
@@ -31,6 +32,8 @@ export default function BookingDetail() {
     const navigate = useNavigate();
 
     if (isLoading) return <Spinner />;
+
+    if (!booking) return <Empty resourceName="booking" />;
 
     const { id, status } = booking;
     const statusToTagName = {
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App.jsx";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 import GlobalStyle from "./styles/GlobalStyles.js";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -18,28 +20,33 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <DarkModeProvider>
-            <QueryClientProvider client={queryClient}>
-                <ReactQueryDevtools />
-                <GlobalStyle />
-                <Toaster
-                    position="top-center"
-                    gutter={12}
-                    containerStyle={{ margin: "8px" }}
-                    toastOptions={{
-                        success: { duration: 3000 },
-                        error: { duration: 5000 },
-                        style: {
-                            fontSize: "16px",
-                            maxWidth: "500px",
-                            padding: "16px 24px",
-                            backgroundColor: "var(--color-grey-0)",
-                            color: "var(--color-grey-700",
-                        },
-                    }}
-                />
-                <App />
-            </QueryClientProvider>
-        </DarkModeProvider>
+        <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            onReset={() => window.location.replace("/")}
+        >
+            <DarkModeProvider>
+                <QueryClientProvider client={queryClient}>
+                    <ReactQueryDevtools />
+                    <GlobalStyle />
+                    <Toaster
+                        position="top-center"
+                        gutter={12}
+                        containerStyle={{ margin: "8px" }}
+                        toastOptions={{
+                            success: { duration: 3000 },
+                            error: { duration: 5000 },
+                            style: {
+                                fontSize: "16px",
+                                maxWidth: "500px",
+                                padding: "16px 24px",
+                                backgroundColor: "var(--color-grey-0)",
+                                color: "var(--color-grey-700",
+                            },
+                        }}
+                    />
+                    <App />
+                </QueryClientProvider>
+            </DarkModeProvider>
+        </ErrorBoundary>
     </React.StrictMode>
 );
